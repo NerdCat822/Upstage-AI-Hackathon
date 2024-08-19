b697755427ba0cbe7e827324b537dae59458237a
김현규 <79504450+NerdCat822@users.noreply.github.com>
2024-08-20 03:04:46 +0900

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,13 +1,12 @@
-# backend.py
 from pydantic import BaseModel
 from Func import RAG
 
-# InputData 클래스를 통해 데이터를 받아 처리하는 것처럼 구현
+# InputData By Pydantic
 class InputData(BaseModel):
     occupation: str
     prompt: str
 
-# FastAPI의 엔드포인트 함수를 일반 함수로 변경
+# Change FastAPI's endpoint function to a regular function
 def process_rag(data: InputData):
     response = RAG(data.prompt, data.occupation)
     return {"response": response}
